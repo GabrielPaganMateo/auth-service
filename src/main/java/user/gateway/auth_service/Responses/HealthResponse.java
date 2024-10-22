5bc8cf82cb25b0232b9f20ea14e3f6d682ebd20e
GabrielPaganMateo <gabriel.pagan.mateo@gmail.com>
2024-10-21 21:47:28 -0400

    Use lombok extension, exchange @Getter @Setter for @Data

--- src/main/java/user/gateway/auth_service/Responses/HealthResponse.java
+++ src/main/java/user/gateway/auth_service/Responses/HealthResponse.java
@@ -3,12 +3,10 @@ package user.gateway.auth_service.Responses;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import user.gateway.auth_service.Constants.HealthMessages;
 
-@Getter
-@Setter
+@Data
 @Component
 public class HealthResponse {
 
